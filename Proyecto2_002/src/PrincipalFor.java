d74f84cc9ffbcdc6ff18d3b5d6362153dae3a289
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-08-27 15:18:22 -0400

    update

---
+++ Proyecto2_002/src/PrincipalFor.java
@@ -0,0 +1,15 @@
+
+public class PrincipalFor {
+    public static void main(String[] args) {
+        
+        for(int i = 1; i <= 10; i++)
+        {
+            System.out.println("Numero: " + i);
+        }
+        int x = 0;
+        for(;x < 10;)
+        {
+            System.out.println(" valor de x: " + (++x));
+        }
+    }
+}
