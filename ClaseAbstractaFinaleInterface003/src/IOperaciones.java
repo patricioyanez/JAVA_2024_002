ae1028aac021dfb668b8797f1e567609030f53a9
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-10-08 18:50:02 -0300

    update

--- ClaseAbstractaFinaleInterface003/src/IOperaciones.java
+++
@@ -1,6 +0,0 @@
-public interface IOperaciones {
-    int COSTO_MINIMO_COMIDA = 10000;
-    
-    int grabar();
-    void limpiar();
-}
--- ClaseAbstractaFinaleInterface003/src/PruebaString.java
+++
@@ -1,5 +0,0 @@
-
-public class PruebaString //extends String 
-{
-    
-}
