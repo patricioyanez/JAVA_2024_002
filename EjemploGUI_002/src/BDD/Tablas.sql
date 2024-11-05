86aa2f3e45101ce022091a32dd9aca0e6e52cda6
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-11-05 15:26:07 -0300

    update

--- EjemploGUI_002/src/BDD/Tablas.sql
+++ EjemploGUI_002/src/BDD/Tablas.sql
@@ -16,6 +16,23 @@ CREATE TABLE Categoria(
 );
 
 
+CREATE TABLE Producto(
+    id          int primary key auto_increment,
+    idMarca     int             not null,
+    idCategoria int             not null,
+    codigo      bigint          not null,
+    descripcion varchar(100)    not null,
+    stock       int             not null,
+    precioCosto int             not null,
+    precioVenta int             not null,
+    UNIQUE(codigo),
+    FOREIGN KEY (idMarca)
+        REFERENCE Marca(id),
+    FOREIGN KEY (idCategoria)
+        REFERENCE Categoria(id)
+);
+
+
 INSERT INTO Marca (nombre, habilitado) values ('Pepsi', 1);
 INSERT INTO Marca (nombre, habilitado) values ('Coca Cola', 1);
 INSERT INTO Marca (nombre, habilitado) values ('Bilz', 1);
